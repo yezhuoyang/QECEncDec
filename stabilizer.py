5798e46770fb3476f35a4ff8d0f001e113282973
Zhuoyang ye <737475845@qq.com>
2024-09-01 17:01:07 -0700

    Negative stabilizer

--- stabilizer.py
+++ stabilizer.py
@@ -1,10 +1,22 @@
 import numpy as np
 
+
+
+def filter_non_zero_values(state_dict):
+    """
+    Filters a dictionary to keep only keys with non-zero values.
+
+    :param state_dict: Dictionary to filter.
+    :return: Filtered dictionary with only non-zero values.
+    """
+    return {key: value for key, value in state_dict.items() if value != 0}
+
+
 class QuantumState:
 
     def __init__(self,num_qubit):
         self._num_qubit=num_qubit
-        self._state_vec={"0"*num_qubit:1}
+        self._state_vec = {format(i, f'0{num_qubit}b'): 1 for i in range(2 ** num_qubit)}
 
     def show_state(self):
         for key in self._state_vec.keys():
@@ -21,15 +33,19 @@ class QuantumState:
     '''
     Multiply I+XZX
     '''
-    def enc_stablizer(self,String):
+    def act_stablizer(self,String):
         new_state_vec={}
+        negative=False
+        if(String[0]=="-"):
+            String=String[1:]
+            negative=True
         for key in self._state_vec.keys():
             if key in new_state_vec.keys():
                 new_state_vec[key]=new_state_vec[key]+self._state_vec[key]
             else:
                 new_state_vec[key]=self._state_vec[key]
             newstatekey=""
-            sign=1
+            sign=1 if negative==False else -1
             for index in range(0,len(key)):
                 if(String[index]=="I"):
                     newstatekey=newstatekey+key[index]
@@ -45,21 +61,15 @@ class QuantumState:
             if newstatekey in new_state_vec.keys(): 
                 new_state_vec[newstatekey]=new_state_vec[newstatekey]+sign*self._state_vec[key]
             else:
-                new_state_vec[newstatekey]=sign*self._state_vec[key]           
+                new_state_vec[newstatekey]=sign*self._state_vec[key]                  
         self._state_vec=new_state_vec
 
 
-    def enc_stabilizer_group(self,stabilizer):
+    def act_stabilizer_group(self,stabilizer):
         for stabstring in stabilizer._stringList:
-            self.enc_stablizer(stabstring)
+            self.act_stablizer(stabstring)
         self.normalize()
-
-    '''
-    check whether sthe stabilizer stabilize the state
-    '''    
-    def check_stablizer(self,String):
-        pass
-
+        self._state_vec=filter_non_zero_values(self._state_vec)
 
 
 class Stabilizer:
